f7287e0a95105cf8278f135f4e8284f62482de98
bmika1 <bartek.mika1@gmail.com>
2017-11-25 18:06:10 +0000

    huj do dupy

--- lcd_handler.py
+++ lcd_handler.py
@@ -1,5 +1,6 @@
 from Adafruit_CharLCD import Adafruit_CharLCD
 import rss_parser
+import time
 
 class LcdHanlder:
 
@@ -7,11 +8,18 @@ class LcdHanlder:
         self.lcd = Adafruit_CharLCD(rs=26, en=19,
                                d4=13, d5=6, d6=5, d7=11,
                                cols=16, lines=2)
-        self.rss = rss_parser.RssParser()
+        self.r_parser = rss_parser.RssParser()
 
 
     def display_rss(self):
 
+        to_display = self.r_parser.refresh_reuters()
         self.lcd.clear()
-        self.lcd.message(self.rss)
-        
+        self.lcd.message(to_display)
+        for x in range(len(to_display)):
+            self.lcd.move_left()
+            time.sleep(1)
+
+
+
+
