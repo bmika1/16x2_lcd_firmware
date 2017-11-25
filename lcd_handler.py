64f0d359317b08eada45f973436a496c7c10d9f8
bmika1 <bartek.mika1@gmail.com>
2017-11-25 18:00:11 +0000

    first commit, rss should work

---
+++ lcd_handler.py
@@ -0,0 +1,17 @@
+from Adafruit_CharLCD import Adafruit_CharLCD
+import rss_parser
+
+class LcdHanlder:
+
+    def __init__(self):
+        self.lcd = Adafruit_CharLCD(rs=26, en=19,
+                               d4=13, d5=6, d6=5, d7=11,
+                               cols=16, lines=2)
+        self.rss = rss_parser.RssParser()
+
+
+    def display_rss(self):
+
+        self.lcd.clear()
+        self.lcd.message(self.rss)
+        
---
+++ rss_parser.py
@@ -0,0 +1,19 @@
+import feedparser
+
+
+class RssParser:
+
+    def refresh_reuters(self):
+
+        reuters_topnews = feedparser.parse("http://feeds.reuters.com/reuters/topNews")['entries']
+
+        reuters_string = ''
+
+        for entry in reuters_topnews:
+            reuters_string = reuters_string + entry['title'] + ';  '
+
+
+        #print reuters_string
+
+        return reuters_string
+
