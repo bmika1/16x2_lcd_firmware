1265b0d8a0a4512839417ac077c47dcc3313d07a
bmika1 <bartek.mika1@gmail.com>
2017-11-26 17:34:12 +0000

    asodhasiod

--- lcd_handler.py
+++ lcd_handler.py
@@ -5,9 +5,6 @@ from datetime import datetime
 
 class LcdHandler:
 
-    _CURSOR_TOP = (0, 0)
-    _CURSOR_BOTTOM = (0, 1)
-
     def __init__(self):
         self.lcd = Adafruit_CharLCD(rs=26, en=19,
                                     d4=13, d5=6, d6=5, d7=11,
@@ -25,17 +22,17 @@ class LcdHandler:
         while True:
             visible_message = "                "
             self.lcd.clear()
-            self.lcd.set_cursor(LcdHandler._CURSOR_BOTTOM)
+            self.lcd.set_cursor(0, 1)
             self.lcd.message(visible_message)
             time.sleep(sleep_time)
             for loc in range(len(bottom_text)):
                 self.lcd.clear()
-                self.lcd.set_cursor(LcdHandler._CURSOR_TOP)
+                self.lcd.set_cursor(0, 0)
                 self.lcd.message(self.get_time(time_format))
-                self.lcd.set_cursor(LcdHandler._CURSOR_BOTTOM)
+                self.lcd.set_cursor(0, 1)
                 visible_message = self.scroll_string(visible_message, bottom_text, loc)
                 self.lcd.message(visible_message)
-                self.lcd.set_cursor(LcdHandler._CURSOR_TOP)
+                self.lcd.set_cursor(0, 0)
                 time.sleep(sleep_time)
 
     def scroll_string(self, visible_message, full_message, loc, scrollby=1):
