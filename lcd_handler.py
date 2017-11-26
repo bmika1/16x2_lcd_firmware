077ef568efe1257d9e2170ad8e97313dfb9b8055
bmika1 <bartek.mika1@gmail.com>
2017-11-26 17:13:00 +0000

    hujd od dypy

--- lcd_handler.py
+++ lcd_handler.py
@@ -5,51 +5,45 @@ from datetime import datetime
 
 class LcdHandler:
 
+    _CURSOR_TOP = (0, 0)
+    _CURSOR_BOTTOM = (0, 1)
+    huj = 1
+
+
     def __init__(self):
         self.lcd = Adafruit_CharLCD(rs=26, en=19,
-                               d4=13, d5=6, d6=5, d7=11,
-                               cols=16, lines=2)
+                                    d4=13, d5=6, d6=5, d7=11,
+                                    cols=16, lines=2)
         self.r_parser = rss_parser.RssParser()
 
-
     def display_time(self):
-        self.lcd.set_cursor(0,0)
-        self.lcd.message(datetime.now().strftime('%b %d  %H:%M:%S\n'))
-        self.lcd.set_cursor(0,0) #Todo: check if this is needed
-
+        global _CURSOR_TOP
+        global _CURSOR_BOTTOM
+        self.lcd.set_cursor(_CURSOR_TOP)
+        self.lcd.message(datetime.now().strftime('%b %d  %H:%M:%S'))
 
     #should be: top, bottom, scrolltop, scrollbottom, scrolinterval
-    def display_rss(self, time_on_top):
+    def display_string(self, time_format, bottom_text, scroll_speed=9):
+        global _CURSOR_TOP
+        global _CURSOR_BOTTOM
 
-        full_message = self.r_parser.refresh_reuters()
+        sleep_time = 1.1-(0.1*scroll_speed)
+
         while True:
-            #message_buffer = ""
-            visible_message = full_message[:16]
+            visible_message = "                "
             self.lcd.clear()
-            self.lcd.set_cursor(0,1)
+            self.lcd.set_cursor(_CURSOR_BOTTOM)
             self.lcd.message(visible_message)
-            time.sleep(0.2)
-            for loc in range(len(full_message)):
+            time.sleep(sleep_time)
+            for loc in range(len(bottom_text)):
                 self.lcd.clear()
-                if time_on_top is True:
-                    self.display_time()
-                self.lcd.set_cursor(0,1)
-                visible_message = self.scroll_string(visible_message, full_message, loc)
+                self.display_time(time_format)
+                self.lcd.set_cursor(_CURSOR_BOTTOM)
+                visible_message = self.scroll_string(visible_message, bottom_text, loc)
                 self.lcd.message(visible_message)
-                self.lcd.set_cursor(0,0)
-                time.sleep(0.2)
+                self.lcd.set_cursor(_CURSOR_BOTTOM)
+                time.sleep(sleep_time)
 
     def scroll_string(self, visible_message, full_message, loc, scrollby=1):
-        visible_message = visible_message[scrollby:]+full_message[loc+16:scrollby+loc+16]
+        visible_message = visible_message[scrollby:]+full_message[loc:scrollby+loc]
         return visible_message
-
-    def load_buffer(self, buffer,  start, full_message):
-        if len(buffer) >= 24:
-	    buffer = buffer[8:]
-        if start == 0:
-            end = 16
-        else:
-            end = start + 8
-        buffer = buffer + full_message[start:end]
-
-        return buffer
