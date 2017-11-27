7945fa54a714b4fea086162b58957f0060fdc108
bmika1 <bartek.mika1@gmail.com>
2017-11-27 04:51:44 +0000

    asd

--- driver.py
+++ driver.py
@@ -1,20 +1,20 @@
 import lcd_handler
 import rss_parser
 from flask import Flask, request, render_template
+import thread
+
 app = Flask(__name__)
 
 lcdh = lcd_handler.LcdHandler()
 rssp = rss_parser.RssParser()
 address = "http://feeds.bbci.co.uk/news/uk/rss.xml?edition=uk"
+_update_request = False
 
-
-while True:
+def display_on_screen():
     rss_str = rssp.refresh_channel(address)
     #time_format = '%H:%M:%S.%f'`'`
     time_format =  '%b %d  %H:%M:%S'
-
     lcdh.display_string(time_format, rss_str, 9, 1)
-
 
 @app.route('/', methods=['POST'])
 def update_rss():
@@ -28,5 +28,7 @@ def display_template():
 
 
 if __name__ == '__main__':
+    thread.start_new_thread(display_on_screen())
     app.run(debug=True, host='0.0.0.0')
    
+
